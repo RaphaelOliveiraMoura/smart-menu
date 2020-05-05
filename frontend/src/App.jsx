d29b52bb12934afc350fa90d5c505696dd229bff
RaphaelOliveiraMoura <raphaeldeoliveiramoura@gmail.com>
2020-05-05 15:17:27 -0300

    chore: start frontend style guide setup
    - add ESLint + Prettier + EditorConfig
    - start app routing setup
    - add material ui dependence

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Router } from 'react-router-dom';
+
+import Routes from './Routes';
+import history from './services/history';
+
+function App() {
+  return (
+    <Router history={history}>
+      <Routes />
+    </Router>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/MenuList/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function MenuList() {
+  return <div>Menu List</div>;
+}
+
+export default MenuList;
